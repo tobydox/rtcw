d563c36cdd9df1fb95d162786271227c00630444
Tobias Doerffel <tobias.doerffel@gmail.com>
2011-08-13 01:20:34 +0200

    Renderer/Shader: revert 0bd10fee as it's causing wrong colors

--- src/renderer/tr_shader.c
+++ src/renderer/tr_shader.c
@@ -983,7 +983,7 @@ static qboolean ParseStage( shaderStage_t *stage, char **text ) {
 	}
 
 	// decide which agens we can skip
-	if ( stage->alphaGen == AGEN_IDENTITY ) {
+	if ( stage->alphaGen == (int) CGEN_IDENTITY ) {
 		if ( stage->rgbGen == CGEN_IDENTITY
 			 || stage->rgbGen == CGEN_LIGHTING_DIFFUSE ) {
 			stage->alphaGen = AGEN_SKIP;
@@ -1895,7 +1895,7 @@ static qboolean CollapseMultitexture( void ) {
 			return qfalse;
 		}
 	}
-	if ( stages[0].alphaGen == AGEN_WAVEFORM ) {
+	if ( stages[0].alphaGen == (int) CGEN_WAVEFORM ) {
 		if ( memcmp( &stages[0].alphaWave,
 					 &stages[1].alphaWave,
 					 sizeof( stages[0].alphaWave ) ) ) {
