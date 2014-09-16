9713973a86dff1311fc37f2110b47ef819ccf704
Tobias Doerffel <tobias.doerffel@gmail.com>
2014-09-17 00:05:29 +0200

    QCommon/CVar: fixed memset() call

--- src/qcommon/cvar.c
+++ src/qcommon/cvar.c
@@ -761,7 +761,7 @@ void Cvar_Restart_f( void ) {
 			}
 			// clear the var completely, since we
 			// can't remove the index from the list
-			memset( var, 0, sizeof( var ) );
+			memset( var, 0, sizeof( *var ) );
 			continue;
 		}
 
